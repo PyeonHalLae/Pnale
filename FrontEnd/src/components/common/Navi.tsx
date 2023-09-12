592369afadf1b7c39386cc6817aa591f7547d43f
김동민 <anwjr41@naver.com>
2023-09-12 15:11:57 +0900

    Feat: 커스텀 색상표 추가

--- FrontEnd/src/components/common/Navi.tsx
+++ FrontEnd/src/components/common/Navi.tsx
@@ -1,17 +1,64 @@
 // src/Layout.tsx
 import { Outlet } from "react-router-dom";
+import tw from "tailwind-styled-components";
 
 export default function Navi() {
   return (
     <div>
       <Outlet />
-      <div>
-        <img src="/img/navi/home-gray.png" alt="" />
-        <img src="/img/navi/search-gray.png" alt="" />
-        <img src="/img/navi/trophy-gray.png" alt="" />
-        <img src="/img/navi/recipy-gray.png" alt="" />
-        <img src="/img/navi/user-gray.png" alt="" />
-      </div>
+      <NaviParent>
+        <NaviDiv>
+          <NaviIcon src="/img/navi/home-gray.png" alt="" />
+          <NaviText>홈</NaviText>
+        </NaviDiv>
+        <NaviDiv>
+          <NaviIcon src="/img/navi/search-gray.png" alt="" />
+          <NaviText>검색</NaviText>
+        </NaviDiv>
+        <NaviDiv>
+          <NaviIcon src="/img/navi/trophy-gray.png" alt="" />
+          <NaviText>편의점</NaviText>
+        </NaviDiv>
+        <NaviDiv>
+          <NaviIcon src="/img/navi/recipy-gray.png" alt="" />
+          <NaviText>레시피</NaviText>
+        </NaviDiv>
+        <NaviDiv>
+          <NaviIcon src="/img/navi/user-gray.png" alt="" />
+          <NaviText>마이</NaviText>
+        </NaviDiv>
+      </NaviParent>
     </div>
   );
 }
+
+const NaviParent = tw.div`
+flex
+min-h-[48px]
+max-h-[60px]
+min-w-[360px]
+w-auto
+justify-center
+gap-4
+absolute
+bottom-0
+`;
+
+const NaviDiv = tw.div`
+w-12 
+mb-1
+`;
+
+const NaviIcon = tw.img`
+mb-1
+mx-auto
+w-9
+h-9
+`;
+
+const NaviText = tw.p`
+text-sm
+text-center
+text-commont-text-gray-color
+text-cu
+`;
