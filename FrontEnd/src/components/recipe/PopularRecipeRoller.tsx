3cc4591ccc5fedc3c809949dccf504471e2b25ad
김효인 <bright_bat@naver.com>
2023-09-13 16:58:20 +0900

    feat: PopularRecipeCard 컴포넌트 틀 완성 (#94)

--- FrontEnd/src/components/recipe/PopularRecipeRoller.tsx
+++ FrontEnd/src/components/recipe/PopularRecipeRoller.tsx
@@ -1,18 +1,33 @@
-// import { useState } from "react";
+import PopularRecipeCard from "./PopularRecipeCard";
 
-// interface recipeType {
-//   recipeTitle: string;
-//   recipeImg: string;
-//   viewCnt: number;
-//   likeCnt: number;
-//   commentCnt: number;
-//   userName: string;
-//   userImg: string;
-//   createdDate: string;
-// }
+interface recipeType {
+  recipeTitle: string;
+  recipeImg: string;
+  viewCnt: number;
+  likeCnt: number;
+  commentCnt: number;
+  userName: string;
+  userImg: string;
+  createdDate: string;
+}
 
-const PopularRecipeRoller = () => {
-  return <div></div>;
+const PopularRecipeRoller = ({ popularRecipeList }: { popularRecipeList: recipeType[] }) => {
+  console.log(popularRecipeList);
+  return (
+    <div>
+      <div>인기레시피</div>
+      <div>
+        여기는 인기레시피 카드 리스트
+        {popularRecipeList &&
+          popularRecipeList.map((popularRecipeItem, index) => (
+            <PopularRecipeCard
+              key={popularRecipeItem.recipeTitle + index}
+              recipeInfo={popularRecipeItem}
+            />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default PopularRecipeRoller;
