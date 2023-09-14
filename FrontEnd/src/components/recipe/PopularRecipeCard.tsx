e813cfbbb1d7f399958f74048f154806cbb394c0
김효인 <bright_bat@naver.com>
2023-09-14 17:42:34 +0900

    feat: 레시피 카드컴포넌트 -> RecipeDeatil 라우터 연결

--- FrontEnd/src/components/recipe/PopularRecipeCard.tsx
+++ FrontEnd/src/components/recipe/PopularRecipeCard.tsx
@@ -1,7 +1,8 @@
 // import tw from "tailwind-styled-components";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-interface recipeType {
+interface popularRecipeType {
   recipeTitle: string;
   recipeImg: string;
   viewCnt: number;
@@ -10,32 +11,48 @@ interface recipeType {
   userName: string;
   userImg: string;
   createdDate: string;
+  recipeId: number;
   // 인기레시피용 타입을 지정해야함
   // Intro
-  // 들어가는 재료(상품들)
+  // 들어가는 재료(상품들) 리스트 []
 }
 
-const PopularRecipeCard = ({ recipeInfo }: { recipeInfo: recipeType }) => {
+const PopularRecipeCard = ({ recipeInfo }: { recipeInfo: popularRecipeType }) => {
+  const navigate = useNavigate();
+
+  // 카드 눌렀을 시 레시피 디테일로 이동
+  const navigateHandler = (recipeId: number) => {
+    navigate(`${recipeId}`);
+  };
+
   return (
-    <Container>
+    <Container
+      onClick={() => {
+        navigateHandler(recipeInfo.recipeId);
+      }}
+    >
       <Title>{recipeInfo.recipeTitle}</Title>
       <Img src={recipeInfo.recipeImg} />
       <Content>
         {/* 재료 */}
-        <div>재료들</div>
+        <Ingredients>재료들재료들재료들재료들재료들재료들재료들재료들재료들재료들</Ingredients>
 
         <div>
-          <IconImg src="/public/img/icon/likeIconPink.png" /> 좋아요 {recipeInfo.likeCnt}개
+          <IconImg src="/img/icons/like-icon-pink.png" /> 좋아요 {recipeInfo.likeCnt}개
         </div>
         <div>
-          <IconImg src="/public/img/icon/viewIconPink.png" /> 조회수 {recipeInfo.viewCnt}회
+          <IconImg src="/img/icons/view-icon-pink.png" /> 조회수 {recipeInfo.viewCnt}회
         </div>
         <div>
-          <IconImg src="/public/img/icon/commentIconPink.png" /> 댓글수 {recipeInfo.commentCnt}개
+          <IconImg src="/img/icons/comment-icon-pink.png" /> 댓글수 {recipeInfo.commentCnt}개
         </div>
       </Content>
-      <Intro>냉장고에 채끝살이 없는 자취생들도 맛있는 짜파게티를 먹을 자격이 있다.</Intro>
-      <Tag>호롤로</Tag>
+      <Intro>
+        냉장고에 채끝살이 없는 자취생들도 맛있는 짜파게티를 먹을 자격이 있다.냉장고에 채끝살이 없는
+        자취생들도 맛있는 짜파게티를 먹을 자격이 있다.냉장고에 채끝살이 없는 자취생들도 맛있는
+        짜파게티를 먹을 자격이 있다.
+      </Intro>
+      <Tag>핫 인플루언서</Tag>
     </Container>
   );
 };
@@ -61,7 +78,7 @@ const Container = styled.div`
   background-color: #fff;
   min-width: 22.5rem;
   max-width: 28.125rem;
-  height: 18.75rem;
+  height: 18.125rem;
   margin-top: 0.62rem;
   margin-bottom: 0.625rem;
   display: grid;
@@ -69,7 +86,6 @@ const Container = styled.div`
   grid-auto-rows: 1fr;
   grid-template-columns: 3fr 2fr;
   grid-template-rows: 1fr 3fr 1fr;
-  gap: 0px 0px;
   grid-template-areas:
     "Title Title"
     "Img Content"
@@ -78,21 +94,37 @@ const Container = styled.div`
 
 const Title = styled.div`
   grid-area: Title;
-  font-size: 1.875rem;
+  font-size: 1.625rem;
   font-style: normal;
   font-weight: 500;
-  line-height: normal;
   align-items: center;
-  width: 85%;
-  margin: 0.3rem;
+  width: 70%;
+  margin-top: 0.5rem;
+  margin-left: 1.125rem;
+  margin-right: 0.625rem;
+  /* height: 4rem; */
+  word-wrap: break-word;
+  overflow: clip;
+  display: -webkit-box; // webkit-box
+  -webkit-line-clamp: 2; // 2줄까지
+  -webkit-box-orient: vertical; //...
 `;
 
 const Img = styled.img`
   grid-area: Img;
-  width: 12.75rem;
-  height: 10.5rem;
+  width: 11.875rem;
+  height: 8.75rem;
   margin: auto;
   src: ${(props) => props.src};
+  border-radius: 0.9375rem;
+`;
+
+const Ingredients = styled.div`
+  margin-right: 1rem;
+  overflow: clip;
+  display: -webkit-box; // webkit-box
+  -webkit-line-clamp: 2; // 2줄까지
+  -webkit-box-orient: vertical; //...
 `;
 
 const IconImg = styled.img<{ src: string }>`
@@ -106,7 +138,7 @@ const Content = styled.div`
   display: grid;
   grid-template-rows: 1fr 1fr 1fr 1fr;
   align-items: center;
-  margin: 0.3rem;
+  margin: 0.1rem;
   font-size: 0.8rem;
   div {
     color: rgb(174 176 182);
@@ -118,9 +150,15 @@ const Content = styled.div`
 
 const Intro = styled.div`
   color: rgb(174 176 182);
-  font-size: 0.9375rem;
+  font-size: 0.75rem;
   grid-area: Intro;
-  margin: 0.3rem 0.5rem;
+  height: 2rem;
+  margin-left: 1.125rem;
+  margin-right: 1.125rem;
+  overflow: hidden;
+  display: -webkit-box; // webkit-box
+  -webkit-line-clamp: 2; // 2줄까지
+  -webkit-box-orient: vertical; //...
 `;
 
 const Tag = styled.div`
