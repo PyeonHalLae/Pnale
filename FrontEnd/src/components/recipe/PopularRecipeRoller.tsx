6be912c78962a025232a25cbd2c8848279eaf8c0
김효인 <bright_bat@naver.com>
2023-09-14 17:45:20 +0900

    feat: RecipeCreate, RecipeDetail 라우터 추가

--- FrontEnd/src/components/recipe/PopularRecipeRoller.tsx
+++ FrontEnd/src/components/recipe/PopularRecipeRoller.tsx
@@ -9,23 +9,20 @@ interface recipeType {
   userName: string;
   userImg: string;
   createdDate: string;
+  recipeId: number;
 }
 
 const PopularRecipeRoller = ({ popularRecipeList }: { popularRecipeList: recipeType[] }) => {
-  console.log(popularRecipeList);
   return (
     <div>
-      <div>인기레시피</div>
-      <div>
-        여기는 인기레시피 카드 리스트
-        {popularRecipeList &&
-          popularRecipeList.map((popularRecipeItem, index) => (
-            <PopularRecipeCard
-              key={popularRecipeItem.recipeTitle + index}
-              recipeInfo={popularRecipeItem}
-            />
-          ))}
-      </div>
+      {/* 여기는 인기레시피 카드 리스트 */}
+      {popularRecipeList &&
+        popularRecipeList.map((popularRecipeItem, index) => (
+          <PopularRecipeCard
+            key={popularRecipeItem.recipeTitle + index}
+            recipeInfo={popularRecipeItem}
+          />
+        ))}
     </div>
   );
 };
