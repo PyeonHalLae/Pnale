93bc3a700a87e6b49a94bfb7c96b9e80f042edf2
김동민 <anwjr41@naver.com>
2023-09-15 17:42:02 +0900

    Feat: 메인페이지 내용 채우기

--- FrontEnd/src/components/main/mainRecipe/MainRecipeHeader.tsx
+++ FrontEnd/src/components/main/mainRecipe/MainRecipeHeader.tsx
@@ -1,5 +1,40 @@
+import tw from "tailwind-styled-components";
+
 const MainRecipeHeader = () => {
-  return <div>MainProductHeader</div>;
+  return (
+    <Box>
+      <Divider></Divider>
+      <Recipe>편의점 추천 레시피</Recipe>
+      <ReMore>더보기</ReMore>
+    </Box>
+  );
 };
 
 export default MainRecipeHeader;
+
+const Divider = tw.div`
+w-[120px] h-1.5 mb-2.5 ml-3 bg-common-peach
+`;
+
+const Box = tw.div`
+relative
+max-w-[450px]
+h-[70px]
+py-2.5
+bg-white
+`;
+
+const Recipe = tw.span`
+text-2xl
+font-bold
+text-common-text-color
+mx-3
+`;
+
+const ReMore = tw.button`
+mr-3
+absolute
+right-0
+text-base
+text-common-peach
+`;
