89f284136956c88068d3f478ef79bdf5462d6329
김동민 <anwjr41@naver.com>
2023-09-18 15:06:19 +0900

    Feat: 메인 화면 레시피 사진 배율 조정...

--- FrontEnd/src/components/main/mainRecipe/MainRecipeContent.tsx
+++ FrontEnd/src/components/main/mainRecipe/MainRecipeContent.tsx
@@ -4,16 +4,16 @@ const MainRecipeContent = () => {
   return (
     <div className="bg-white border-2 border-white">
       <div className="m-3 border-2 shadow-xl border-common-white-divider">
-        <img src="/img/test/recipeTest.png" alt="레시피 사진" className=""></img>
+        <img src="/img/test/recipeTest.png" alt="레시피 사진" className="h-[210px] w-full"></img>
         <TextBox>
           <div className="w-10/12 text-common-text-color">
             <Title>짜파구리</Title>
             <Tag>#짜파게티</Tag>
             <Tag>#너구리</Tag>
           </div>
-          <div>
+          <div className="my-auto">
             <Profile src="/img/test/profileTest.png" alt="프로필사진" />
-            <span className="font-[G-sans-light] text-xl">김동민</span>
+            <span className="font-[G-sans-light] text-base">김동민</span>
           </div>
         </TextBox>
       </div>
@@ -26,21 +26,21 @@ export default MainRecipeContent;
 const TextBox = tw.div`
 relative
 mx-5
-my-3
+my-2
 flex
 `;
 
 const Title = tw.div`
-text-4xl
-mt-2
+text-3xl
+mt-1
 mb-1
 `;
 
 const Tag = tw.span`
 font-[G-sans-light]
-text-2xl
+text-xl
 `;
 const Profile = tw.img`
-w-14
+w-10
 mb-1
 `;
