5f6ef258082027bbf57a99ded2ed4ab50767ea31
김효인 <bright_bat@naver.com>
2023-09-19 15:01:04 +0900

    refactor: 레시피메인페이지 컴포넌트 디렉토리 변경

--- FrontEnd/src/components/recipe/RecipeDetail.tsx
+++ FrontEnd/src/components/recipe/RecipeDetail.tsx
@@ -1,14 +1,31 @@
 import CommentBox from "./RecipeDetail/CommentBox";
 import { useParams } from "react-router-dom";
+import RecipeDetailContent from "./RecipeDetail/RecipeDetailContent";
+import RelatedVideo from "./RecipeDetail/RelatedVideo";
+import IngredientsList from "./RecipeDetail/IngredientsList";
+import tw from "tailwind-styled-components";
+import RecipeDetailHeader from "./RecipeDetail/RecipeDetailHeader";
 
 const RecipeDetail = () => {
   const { recipeId } = useParams();
   return (
     <div>
-      RecipeDetail {recipeId}
+      {recipeId}
+      <PageHeader> 삭제 수정 버튼이 들어가야함 </PageHeader>
+      <MainImg />
+      <RecipeDetailHeader />
+      <IngredientsList />
+      <RelatedVideo videoUrl="ㅎㅎ" />
+      <RecipeDetailContent content="이거이거 ㅎ" />
       <CommentBox />
     </div>
   );
 };
 
 export default RecipeDetail;
+
+const MainImg = tw.img`
+`;
+
+const PageHeader = tw.div`
+`;
