2b54d89d2af2bc0f83563591187aa63a9bc999fb
김효인 <bright_bat@naver.com>
2023-09-20 15:53:30 +0900

    feat: 레시피 디테일 페이지 관련 영상 컴포넌트

--- FrontEnd/src/components/recipe/RecipeDetail.tsx
+++ FrontEnd/src/components/recipe/RecipeDetail.tsx
@@ -13,9 +13,9 @@ const RecipeDetail = () => {
       {recipeId}
       <PageHeader> 삭제 수정 버튼이 들어가야함 </PageHeader>
       <MainImg />
-      <RecipeDetailHeader />
+      <RecipeDetailHeader mainImgUrl={"/img/test/너굴맨레시피.jpg"} />
       <IngredientsList />
-      <RelatedVideo videoUrl="ㅎㅎ" />
+      <RelatedVideo videoUrl="https://www.youtube.com/watch?v=oCduiXGcWdw" />
       <RecipeDetailContent content="이거이거 ㅎ" />
       <CommentBox />
     </div>
--- FrontEnd/src/components/recipe/recipeDetailComponent/RelatedVideo.tsx
+++ FrontEnd/src/components/recipe/recipeDetailComponent/RelatedVideo.tsx
@@ -1,10 +1,46 @@
 import tw from "tailwind-styled-components";
 
 const RelatedVideo = ({ videoUrl }: { videoUrl: string }) => {
+  // const ThuUrl = videoUrl
+  //   .replace("https://youtu.be/", "")
+  //   .replace("https://www.youtube.com/embed/", "")
+  //   .replace("https://www.youtube.com/watch?v=", "")
+  //   .split("&")[0];
+
+  const LinkUrl = videoUrl
+    .replace("https://youtu.be/", "https://www.youtube.com/embed/")
+    .replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
+
   return (
     <Container>
-      <Header>관련 영상</Header>
-      <VideoThumbnail>{videoUrl}여기는 이미지</VideoThumbnail>
+      <Header>
+        관련 영상
+        <a
+          href={videoUrl}
+          className="w-[100%]
+          text-[.75rem] 
+          line-clamp-1 
+          mb-[.625rem] 
+          p-[.25rem]
+          border-[0.05rem]
+          rounded-[0.5rem]
+          border-common-bold-back-color
+          bg-common-back-color"
+        >
+          {videoUrl}
+        </a>
+      </Header>
+      <VideoBox>
+        {/* <a rel="stylesheet" href={videoUrl}>
+          <img
+            className="w-full"
+            src={`https://img.youtube.com/vi/${ThuUrl}/mqdefault.jpg`}
+            alt="보러가기"
+          />
+        </a> */}
+
+        <iframe className="w-[calc(100%)] h-[18.75rem]" src={LinkUrl} title="정의"></iframe>
+      </VideoBox>
     </Container>
   );
 };
@@ -19,6 +55,6 @@ const Header = tw.div`
 p-[1rem] text-[1.5rem] text-common-text-color
 `;
 
-const VideoThumbnail = tw.div`
+const VideoBox = tw.div`
 p-[1rem] pt-0
 `;
