98b8056dd05ec4dd7e784740ac996a75bd8d96b4
이현욱 <dldnr789@gmail.com>
2023-09-25 08:36:16 +0900

    feat. 편의점별 api init

--- BackEnd/src/main/java/com/ssafy/special/CSR/controllers/ConvController.java
+++ BackEnd/src/main/java/com/ssafy/special/CSR/controllers/ConvController.java
@@ -1,13 +1,20 @@
 package com.ssafy.special.CSR.controllers;
 
 import com.ssafy.special.CSR.services.ConvService;
+import com.ssafy.special.enums.CorpType;
+import com.ssafy.special.exception.DataResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value ={"/api/conv", "/api/auth/conv"} )
 @RequiredArgsConstructor
 public class ConvController {
     private final ConvService convService;
+
+    @GetMapping("/{corpType}")
+    public DataResponse<?> getConvData(@PathVariable("corpType") CorpType corpType){
+        return new DataResponse<>(200, corpType.name() + "의 데이터를 반환합니다.");
+    }
+
 }
