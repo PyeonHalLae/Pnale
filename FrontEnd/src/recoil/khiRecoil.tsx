f100d5aa3fb57f50360750223e4d3c4b08f7b765
김효인 <bright_bat@naver.com>
2023-09-27 14:56:35 +0900

    feat: 레시피 작성페이지 recoil 활용

--- FrontEnd/src/recoil/khiRecoil.tsx
+++ FrontEnd/src/recoil/khiRecoil.tsx
@@ -1,6 +1,27 @@
 import { atom } from "recoil";
+import {
+  recipeFormType,
+  productFormType,
+} from "./../components/recipe/recipeCommonComponent/recipeFormType";
 
-export const ingredientState = atom({
-  key: "ingredientState",
+export const recipeFormState = atom<recipeFormType>({
+  key: "recipeFormState",
+  default: {
+    recipeTitle: "",
+    intro: "",
+    relatedUrl: "",
+  },
+});
+
+export const recipeImgState = atom<string>({
+  key: "recipeImgState",
   default: "",
+});
+export const recipeContentsState = atom<string>({
+  key: "recipeContentsState",
+  default: "",
+});
+export const recipeProductsState = atom<productFormType[]>({
+  key: "recipeProductsState",
+  default: [],
 });
