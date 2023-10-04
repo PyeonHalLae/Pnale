6fc76176aab2bc368ee83eeb3a2907f73d8596af
gmzuddl <dldnr789@koreatech.ac.kr>
2023-10-05 00:40:00 +0900

    temp. 상품검색 redis 제한

--- BackEnd/src/main/java/com/ssafy/special/CSR/controllers/ElasticController.java
+++ BackEnd/src/main/java/com/ssafy/special/CSR/controllers/ElasticController.java
@@ -54,7 +54,7 @@ public class ElasticController {
         if(requestData.getIds().size() == 0) throw new CustomException(CustomErrorCode.NO_SEARCH_DATA) ;
         Long memberId = (Long) request.getAttribute("memberId");
 
-        redisService.setSearchList(memberId, requestData.getKeyword());
+        //redisService.setSearchList(memberId, requestData.getKeyword());
 
         log.info("{}, 멤버{}", requestData.getIds().size(), memberId);
         return new DataResponse<>(200, "검색 결과를 반환합니다.", goodsService.findIdsResult(pageable, requestData.getIds(), memberId ));
