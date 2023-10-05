adf820733cde13397accfee9153baf90dfff3e78
김효인 <bright_bat@naver.com>
2023-10-06 02:09:57 +0900

    feat: 레시피 생성, 수정시 로그인여부/본인여부 프론트 유효성 검사

--- FrontEnd/src/components/recipe/RecipeCreate.tsx
+++ FrontEnd/src/components/recipe/RecipeCreate.tsx
@@ -13,15 +13,46 @@ import {
   recipeFormImg,
   recipeFormContent,
 } from "@/recoil/khiRecoil";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { ToastErrorMessage } from "@/model/toastMessageJHM";
 
 const RecipeCreate = () => {
   const [step, setStep] = useState<string>("1");
+  const navigate = useNavigate();
   const resetForm = useResetRecoilState(recipeFormState);
   const restContents = useResetRecoilState(recipeFormContent);
   const restImg = useResetRecoilState(recipeFormImg);
   const restProducts = useResetRecoilState(recipeFormProduct);
 
   useEffect(() => {
+    axios
+      .get("/api/member/login", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      })
+      .then(() => {})
+      .catch((error) => {
+        const code = error.response.status;
+        if (code === 401) {
+          // 액세스토큰이 있으나 유효하지 않은 경우
+          axios
+            .get("/api/auth/member/login", {
+              withCredentials: true,
+            })
+            .then(() => {})
+            .catch(() => {
+              ToastErrorMessage("로그인이 필요합니다");
+              navigate("/sociallogin");
+            });
+        } else {
+          ToastErrorMessage("로그인이 필요합니다");
+          navigate("/sociallogin");
+        }
+      });
+
     resetForm();
     restContents();
     restImg();
